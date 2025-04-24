4bbbd2c5edb96dc67f3dd5ee363991c5c4943390
Phuz <trieuphudai190@gmail.com>
2025-04-24 17:28:46 +0700

    Home Fragment: Slide Banner

--- app/src/main/java/com/example/doan/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/doan/Fragment/HomeFragment.java
@@ -3,13 +3,17 @@ package com.example.doan.Fragment;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.ItemClickListener;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.doan.R;
 import com.example.doan.databinding.FragmentHomeBinding;
@@ -19,6 +23,8 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private FragmentHomeBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +33,37 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        com.example.doan.databinding.FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        var imageList = new ArrayList<SlideModel>();
+        imageList.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
+
+        var imageSlider = binding.imageSlider;
+        imageSlider.setImageList(imageList);
+        imageSlider.setImageList(imageList, ScaleTypes.FIT);
+
+
+        imageSlider.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void doubleClick(int i) {
+
+            }
+
+            @Override
+            public void onItemSelected(int position) {
+                // xử lý gì đó với item
+                String itemMessage = "Selected image " + (position + 1);
+                Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
