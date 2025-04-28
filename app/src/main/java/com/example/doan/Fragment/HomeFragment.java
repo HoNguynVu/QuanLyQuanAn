7d786cfcc75621b082707c917b931af9895bee6f
Phuz <trieuphudai190@gmail.com>
2025-04-28 15:11:51 +0700

    Home Fragment: Thêm NestedScrollView, RecyclerView hiển thị Popular Item

--- app/src/main/java/com/example/doan/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/doan/Fragment/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.transition.Slide;
 import android.view.LayoutInflater;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.interfaces.ItemClickListener;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.doan.Adapter.PopularItemAdapter;
 import com.example.doan.R;
 import com.example.doan.databinding.FragmentHomeBinding;
 
@@ -23,17 +26,54 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    List<String> titles;
+    List<String> prices;
+    List<Integer> images;
+    PopularItemAdapter adapter;
     private FragmentHomeBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 
+    private void addAllTitles() {
+        titles = new ArrayList<>();
+        titles.add("Pizza");
+        titles.add("Burger");
+        titles.add("Hotdog");
+        titles.add("Drink");
+    }
+
+    private void addAllPrices() {
+        prices = new ArrayList<>();
+        prices.add("10$");
+        prices.add("10$");
+        prices.add("10$");
+        prices.add("10$");
+    }
+
+    private void addAllImages() {
+        images = new ArrayList<>();
+        images.add(R.drawable.celery_soup);
+        images.add(R.drawable.dimsum_soup);
+        images.add(R.drawable.kale_soup);
+        images.add(R.drawable.mashroom_soup);
+    }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        addAllTitles();
+        addAllPrices();
+        addAllImages();
+        adapter = new PopularItemAdapter(requireContext(), titles, prices, images, getActivity());
+
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         return binding.getRoot();
     }
 
