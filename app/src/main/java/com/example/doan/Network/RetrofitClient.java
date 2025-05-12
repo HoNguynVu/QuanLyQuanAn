0061c4654d53c220f2cb87708064b916e0a19bcb
Ho Nguyen Vu <23521805@gm.uit.edu.vn>
2025-05-12 20:03:29 +0700

    thay doi login

---
+++ app/src/main/java/com/example/doan/Network/RetrofitClient.java
@@ -0,0 +1,19 @@
+package com.example.doan.Network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://restaurantapi.great-site.net/"; // Đảm bảo đúng domain và dấu '/'
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
