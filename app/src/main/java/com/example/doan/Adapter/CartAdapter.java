9a753390bc173eebf534a717ebfab65cc47e93db
Phuz <trieuphudai190@gmail.com>
2025-05-12 16:53:01 +0700

    CartFragment: done

--- app/src/main/java/com/example/doan/Adapter/CartAdapter.java
+++ app/src/main/java/com/example/doan/Adapter/CartAdapter.java
@@ -1,6 +1,7 @@
 package com.example.doan.Adapter;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -8,17 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.doan.databinding.CartItemBinding;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
     private final List<String> cartItems;
     private final List<String> cartItemPrice;
     private final List<Integer> cartImage;
+    private final int[] itemQuantities;
 
     public CartAdapter(List<String> cartItems, List<String> cartItemPrice, List<Integer> cartImage) {
         this.cartItems = cartItems;
         this.cartItemPrice = cartItemPrice;
         this.cartImage = cartImage;
+
+        itemQuantities = new int[cartItems.size()];
+        Arrays.fill(itemQuantities, 1);
     }
 
     @NonNull
@@ -38,17 +44,63 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
         return cartItems.size();
     }
 
-public class CartViewHolder extends RecyclerView.ViewHolder{
-    private final CartItemBinding binding;
-    public CartViewHolder(CartItemBinding binding) {
-        super(binding.getRoot());
-        this.binding = binding;
-    }
+    public class CartViewHolder extends RecyclerView.ViewHolder {
+        private final CartItemBinding binding;
 
-    public void bind(int position) {
-        binding.tvName.setText(cartItems.get(position));
-        binding.tvPrice.setText(cartItemPrice.get(position));
-        binding.imageView.setImageResource(cartImage.get(position));
+        public CartViewHolder(CartItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(int position) {
+            binding.tvName.setText(cartItems.get(position));
+            binding.tvPrice.setText(cartItemPrice.get(position));
+            binding.imageView.setImageResource(cartImage.get(position));
+            binding.cartItemQuantity.setText(String.valueOf(itemQuantities[position]));
+            binding.btnMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    decreaseQuantity(getAdapterPosition());
+                }
+            });
+
+            binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    increaseQuantity(getAdapterPosition());
+                }
+            });
+
+            binding.trash.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(getAdapterPosition() != RecyclerView.NO_POSITION)
+                        deleteItem(getAdapterPosition());
+                }
+            });
+        }
+
+        private void decreaseQuantity(int position) {
+            if (itemQuantities[position] > 1) {
+                itemQuantities[position]--;
+                binding.cartItemQuantity.setText(String.valueOf(itemQuantities[position]));
+            }
+        }
+
+        private void increaseQuantity(int position) {
+            if (itemQuantities[position] < 10) {
+                itemQuantities[position]++;
+                binding.cartItemQuantity.setText(String.valueOf(itemQuantities[position]));
+            }
+        }
+
+
+        private void deleteItem(int position) {
+            cartItems.remove(position);
+            cartImage.remove(position);
+            cartItemPrice.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, cartItems.size());
+        }
     }
-}
 }
