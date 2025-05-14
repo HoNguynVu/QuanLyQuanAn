9c170d5d4b5e1bc1f893d2fbbd41b631356bab27
quangvinh263 <23521792@gm.uit.edu.vn>
2025-05-14 12:43:43 +0700

    Update Retrofit

--- app/src/main/java/com/example/doan/Network/RetrofitClient.java
+++ app/src/main/java/com/example/doan/Network/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.1.65/restaurantapi/"; // dùng 10.0.2.2 thay vì localhost nếu chạy trên Android Emulator
+    private static final String BASE_URL = "http://10.0.2.2:8081/restaurantapi/"; // dùng 10.0.2.2 thay vì localhost nếu chạy trên Android Emulator
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
